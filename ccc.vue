65da69c386a0be4cfba6e40a87f70eff16d79fb9
Lhaha019 <2821445109@qq.com>
2025-05-22 17:00:58 +0800

    修改CCC

---
+++ ccc.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="right">
+		<div class="top-row">
+			<span>风险选择</span>
+		</div>
+		<div class="custom-select">
+			<select >
+				<option value="ALL">全部</option>
+				<option value="BC">冰川冰湖</option>
+				<option value="XP">斜坡变形区</option>
+				<option value="WD">稳定历史变形破坏区</option>
+				<option value="FH">复活历史变形破坏区</option>
+			</select>
+			<span class="select-arrow"></span>
+		</div>
+	</div>
+</template>
